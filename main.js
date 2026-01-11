2809616a7b83b697573bd279db2c154b9ee2334d
Desmond <dnwakpati@gmail.com>
2026-01-11 11:54:15 +0100

    changes made to the navbar

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 const hamburger = document.getElementById("hamburger");
 const navlist = document.querySelector(".navlist");
+const navLinks = document.querySelectorAll(".navlist a");
 
 hamburger.addEventListener("click", () => {
     navlist.classList.toggle("active");
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navlist.classList.remove("active");
+    });
+});
+
+document.addEventListener("click", (e) => {
+    if (
+        !navlist.contains(e.target) &&
+        !hamburger.contains(e.target)
+    ) {
+        navlist.classList.remove("active");
+    }
 });
